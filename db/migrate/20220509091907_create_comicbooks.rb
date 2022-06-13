8109fe220eabdae6f1475fe760847159940a8900
rashmik06 <rashmik@witmates.com>
2022-06-13 15:10:15 +0530

    Add db to main

---
+++ db/migrate/20220509091907_create_comicbooks.rb
@@ -0,0 +1,14 @@
+class CreateComicbooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :comicbooks do |t|
+      t.string :title
+      t.string :description
+      t.integer :publisher_id
+      t.integer :number
+      t.string :artist
+      t.string :writer
+
+      t.timestamps
+    end
+  end
+end
